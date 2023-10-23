5c4e4ae7d3c5ac1ff2584cb6d059952f2bed69f4
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2023-10-23 00:03:35 +0000

    added links

--- Components/Button/BtnBlue.tsx
+++ Components/Button/BtnBlue.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/next-js"
 
 type BtnBlueProps ={
   btnType: string
@@ -11,7 +12,11 @@ export default function BtnBlue(props:BtnBlueProps) {
       borderRadius={"50px"} 
       fontSize={"18px"} fontWeight={"normal"} 
       color={"white"} py={"1.7rem"} px={"1.7rem"} 
-      bgColor={"rgba(52, 97, 255, 1)"}>{props.btnType}</Button>
+      bgColor={"rgba(52, 97, 255, 1)"}>
+        <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSeLVRSChFawU5H543BkRzoPgie5vwwqMMUDRAvM6cz5RlRaEQ/viewform"}>
+        {props.btnType}
+        </Link>
+        </Button>
     </>
   )
 }
