37827f57b4cc4ad210e37a7f2512a9d8b5408350
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2023-10-23 06:19:44 +0000

    added more links

--- Components/Button/BtnYellow.tsx
+++ Components/Button/BtnYellow.tsx
@@ -1,10 +1,16 @@
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { Box, Button } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/next-js"
 
 export default function BtnYellow() {
   return (
     <>
-      <Button py={"1.7rem"} px={"1.7rem"}  bgColor={"rgba(252, 201, 68, 1)"} borderRadius={"50px"} position={"relative"} fontSize={"18px"} color={"white"} rightIcon={<ArrowForwardIcon />} >Apply</Button>
+      <Button py={"1.7rem"} px={"1.7rem"}  bgColor={"rgba(252, 201, 68, 1)"} borderRadius={"50px"} position={"relative"} fontSize={"18px"} color={"white"} rightIcon={<ArrowForwardIcon />} 
+      >
+        <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSeLVRSChFawU5H543BkRzoPgie5vwwqMMUDRAvM6cz5RlRaEQ/viewform"}>
+        Apply
+        </Link>
+        </Button>
     </>
   )
 }
--- Components/Button/NoarrowBtn.tsx
+++ Components/Button/NoarrowBtn.tsx
@@ -1,10 +1,14 @@
-
+import { Link } from "@chakra-ui/next-js"
 import { Button } from "@chakra-ui/react";
 
 export default function NoarrowBtn() {
   return (
     <>
-      <Button py={"1.7rem"} px={"2.3rem"} bgColor={"rgba(252, 201, 68, 1)"} borderRadius={"50px"} fontSize={"18px"} color={"white"} >Apply</Button>
+      <Button py={"1.7rem"} px={"2.3rem"} bgColor={"rgba(252, 201, 68, 1)"} borderRadius={"50px"} fontSize={"18px"} color={"white"} >
+      <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSeLVRSChFawU5H543BkRzoPgie5vwwqMMUDRAvM6cz5RlRaEQ/viewform"}>
+        Apply
+        </Link>
+        </Button>
     </>
   )
 }
