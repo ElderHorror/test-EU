86776cdf4c99af1c989e79afc25440dd9a209cc8
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2023-10-24 01:15:50 +0000

    added some info to the loans

--- Components/Loans/LoansListCarousel.tsx
+++ Components/Loans/LoansListCarousel.tsx
@@ -10,15 +10,17 @@ export default function LoansListCarousel() {
             textt:"This product is designed to help students cover the expenses related to visa processing, making it hassle-free and worry-free. It addresses challenges such as banking restrictions and unfavorable conversion rates, enabling students to secure their study visa without financial constraints.",
             smHeading:"T&C Apply",
             tC:"Repayment Term: 2 months",
-            tCtwo:"Interest rate: 5%"
+            tCtwo:"Interest rate: 5%",
+            loanAmount: ""
         },
         {
             id:"2",
             headingg:"Accommodation Payment Loan",
             textt:"Finding suitable accommodation in a new country is a top concern for students. This loan is dedicated to assisting students in securing comfortable living arrangements, ensuring they have a safe and convenient place to call home during their studies.",
             smHeading:"T&C Apply",
-            tC:"Repayment Term: 2 months",
-            tCtwo:"Interest rate: 5%"
+            tC:"Repayment Term: 3 months",
+            tCtwo:"Interest rate: 8%",
+            loanAmount: ""
         },
         {
             id:"3",
@@ -26,30 +28,34 @@ export default function LoansListCarousel() {
             textt:"Finding suitable accommodation in a new country is a top concern for students. This loan is dedicated to assisting students in securing comfortable living arrangements, ensuring they have a safe and convenient place to call home during their studies.",
             smHeading:"T&C Apply",
             tC:"Repayment Term: 2 months",
-            tCtwo:"Interest rate: 5%"
+            tCtwo:"Interest rate: 15%",
+            loanAmount: ""
         },
         {
             id:"4",
-            headingg:"Accommodation Payment Loan",
-            textt:"Finding suitable accommodation in a new country is a top concern for students. This loan is dedicated to assisting students in securing comfortable living arrangements, ensuring they have a safe and convenient place to call home during their studies.",
+            headingg:"Settler's Safety Net Loan",
+            textt:"The perio between your arrival in a new country and the disbursement of your scholarship stipend can be financially challenging. Our Settler's Saftey Net Loan offers you a finnacial cushion to cover essential expenses such as initial groceries, transportation an other immediate needs. it's your saftey net during those critical early days in your new enviroment. ",
             smHeading:"T&C Apply",
             tC:"Repayment Term: 2 months",
-            tCtwo:"Interest rate: 5%"
+            tCtwo:"Interest rate: 7.5%",
+            loanAmount: "500 euro"
+
         },
         {
             id:"5",
-            headingg:"Accommodation Payment Loan",
-            textt:"Finding suitable accommodation in a new country is a top concern for students. This loan is dedicated to assisting students in securing comfortable living arrangements, ensuring they have a safe and convenient place to call home during their studies.",
+            headingg:"Self Payment",
+            textt:"Do you have the funds to kickstart your academic journey but find yourself grappling with banking restrictins? We can confidently process all your academic fees with minimal charges, even in the face of banking limitations. We will help you handle your finacial respnsibilities effortlessly, so you can focus wholeheartedly on other important things ",
             smHeading:"T&C Apply",
-            tC:"Repayment Term: 2 months",
-            tCtwo:"Interest rate: 5%"
+            tC:"Repayment Term:The processing fee is charged at once ",
+            tCtwo:"Charges: 5 euro per 100 euro ",
+            loanAmount: ""
         }
     ]
   return (
     <Box  
       overflowX={"scroll"}
       // marginLeft={{base:"none",lg:"calc(50% - 50vw)"}}
-      // marginRight={{base:"none",lg:"calc(50% - 50vw)"}}
+      // marginRight={{base:"none",lg:"calc(50% - 50vw)"}
       width={"100vw"}
       css={{
         '&::-webkit-scrollbar':{
--- Components/Loans/LoansProp.tsx
+++ Components/Loans/LoansProp.tsx
@@ -9,6 +9,7 @@ type LoansPropProps ={
         smHeading: string 
         tC: string
         tCtwo: string
+        loanAmount: string
     }[]
 }
 
@@ -22,28 +23,36 @@ export default function LoansProp(props:LoansPropProps) {
                <Box margin={{base:"auto", lg:"none"}} w={{base:200, lg:400}} h={100}>
                  <Heading fontSize={{base:"24px", md:"30px", lg:"48px"}} color={"white"} textAlign={"center"}>{slide.headingg}</Heading>
                </Box>
-                <Box alignSelf={"center"} w={{base:300, md:300, lg:400}}>
+                <Box alignSelf={"center"} w={{base:300, md:300, lg:400}} h={200}>
                     <Text fontSize={"16px"} lineHeight={"30px"} color={"white"}>{slide.textt}</Text>
                 </Box>
 
-                <Box borderRadius={"20px"} w={"316px"} p={"1rem"} bgColor={"rgba(17, 41, 125, 1)"}>
+                <Box borderRadius={"20px"}
+                //  w={"316px"} 
+                w={"auto"}
+                h={200}
+                 p={"1rem"} bgColor={"rgba(17, 41, 125, 1)"}>
                     <Flex flexDir={"column"}>
                      <Box>
                       <Heading fontWeight={600} fontSize={"16.79px"} color={"white"}>{slide.smHeading}</Heading>
                      </Box>
                      <Box>
-                        <Flex alignItems={"center"} mt={".3rem"} gap={2}>
-                            <Box bgColor={"rgba(252, 201, 68, 1)"} width={"15.43px"} borderRadius={"50px"} height={"15.43px"}></Box>
+                        <Flex alignItems={"center"} mt={".3rem"} gap={13}>
+                            <Box bgColor={"rgba(252, 201, 68, 1)"} 
+                            
+                            width={"15.43px"} 
+                            borderRadius={"50px"} height={"15.43px"}></Box>
                             <Box>
                                 <Text color={"white"}>{slide.tC}</Text>
                             </Box>
                         </Flex>
                      </Box>
                      <Box>
-                        <Flex alignItems={"center"} mt={".3rem"} gap={2}>
+                        <Flex alignItems={"center"} mt={".3rem"} flexGrow={""} gap={13}>
                             <Box bgColor={"rgba(252, 201, 68, 1)"} width={"15.43px"} borderRadius={"50px"} height={"15.43px"}></Box>
                             <Box>
                                 <Text color={"white"}>{slide.tCtwo}</Text>
+                                <Text color={"white"}>{slide.loanAmount}</Text>
                             </Box>
                         </Flex>
                      </Box>
