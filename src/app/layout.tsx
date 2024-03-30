9224fa7ef3ece558b4f37d84501334b58f843ef0
xlassix <awhy14539@gmail.com>
2024-03-30 08:26:33 +0100

    required form input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
-        <GoogleAnalytics gaId="G-SHF7JR3ZV1" />
+        <GoogleAnalytics gaId="G-7N6DT3DSEV" />
         <Analytics />
       </body>
     </html>
