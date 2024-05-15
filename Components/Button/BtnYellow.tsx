e0b7f04be76f82a1033264527447a265f2c0d949
xlassix <oayobami15@gmail.com>
2024-05-15 22:31:59 +0100

    refractor: change apply link

--- Components/Button/BtnYellow.tsx
+++ Components/Button/BtnYellow.tsx
@@ -7,7 +7,7 @@ export default function BtnYellow() {
     <>
       <Button py={"1.7rem"} px={"1.7rem"}  bgColor={"rgba(252, 201, 68, 1)"} borderRadius={"50px"} position={"relative"} fontSize={"18px"} color={"white"} rightIcon={<ArrowForwardIcon />} 
       >
-        <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSeLVRSChFawU5H543BkRzoPgie5vwwqMMUDRAvM6cz5RlRaEQ/viewform"}>
+        <Link href={"https://forms.gle/RzhPjxMs5TBQw9Qv9"}>
         Apply
         </Link>
         </Button>
--- Components/Button/NoarrowBtn.tsx
+++ Components/Button/NoarrowBtn.tsx
@@ -5,7 +5,7 @@ export default function NoarrowBtn() {
   return (
     <>
       <Button py={"1.7rem"} px={"2.3rem"} bgColor={"rgba(252, 201, 68, 1)"} borderRadius={"50px"} fontSize={"18px"} color={"white"} >
-        <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSeLVRSChFawU5H543BkRzoPgie5vwwqMMUDRAvM6cz5RlRaEQ/viewform"}>
+        <Link href={"https://forms.gle/RzhPjxMs5TBQw9Qv9"}>
           Apply
         </Link>
         </Button>
