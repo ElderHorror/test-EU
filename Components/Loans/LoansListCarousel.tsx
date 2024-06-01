bfd264763abab3de79c40d2378ea9f9f16f241fb
Olamide Adu <95123969+xrander@users.noreply.github.com>
2024-06-01 06:54:21 +0200

    Typo fix for LoansListCarousel.tsx
    - flight expenses was an exact copy of accommodation
    - multiple typos in settlers loan have been fixed

--- Components/Loans/LoansListCarousel.tsx
+++ Components/Loans/LoansListCarousel.tsx
@@ -26,7 +26,7 @@ export default function LoansListCarousel() {
     {
       id: "3",
       headingg: "Flight Expense Loan",
-      text: "Finding suitable accommodation in a new country is a top concern for students. This loan is dedicated to assisting students in securing comfortable living arrangements, ensuring they have a safe and convenient place to call home during their studies.",
+      text: "Students need to appear in person and that is one of the leading cause of headaches with increasing flight expenses. We'll cover your flight fare and ensure that the cost is not a hindrance to fulfilling your study goals,",
       smHeading: "T&C Apply",
       tC: "Repayment Term: 2 months",
       tCtwo: "Interest rate: 15%",
@@ -35,7 +35,7 @@ export default function LoansListCarousel() {
     {
       id: "4",
       headingg: "Settler's Safety Net Loan",
-      text: "The period between your arrival in a new country and the disbursement of your scholarship stipend can be financially challenging. Our Settler's Saftey Net Loan offers you a finnacial cushion to cover essential expenses such as initial groceries, transportation an other immediate needs. it's your saftey net during those critical early days in your new enviroment. ",
+      text: "The period between your arrival in a new country and the disbursement of your scholarship stipend can be financially challenging. Our Settler's Safety Net Loan offers you a financial cushion to cover essential expenses such as initial groceries, transportation an other immediate needs. it's your safety net during those critical early days in your new environment. ",
       smHeading: "T&C Apply",
       tC: "Repayment Term: 2 months",
       tCtwo: "Interest rate: 7.5%",
@@ -45,7 +45,7 @@ export default function LoansListCarousel() {
     {
       id: "5",
       headingg: "Self Payment",
-      text: "Do you have the funds to kickstart your academic journey but find yourself grappling with banking restrictins? We can confidently process all your academic fees with minimal charges, even in the face of banking limitations. We will help you handle your finacial respnsibilities effortlessly, so you can focus wholeheartedly on other important things ",
+      text: "Do you have the funds to kickstart your academic journey but find yourself grappling with banking restrictins? We can confidently process all your academic fees with minimal charges, even in the face of banking limitations. We will help you handle your financial respnsibilities effortlessly, so you can focus wholeheartedly on other important things ",
       smHeading: "T&C Apply",
       tC: "Repayment Term:The processing fee is charged at once ",
       tCtwo: "Charges: 5 euro per 100 euro ",
